e3f0e06988b6a8655032bc55adf4cb4576f71da4
Jason Chodakowski <jchodakowski@me.com>
2017-01-19 15:21:20 -0500

    Updated version number in 'metadata.rb'

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "nickola@nickola.ru"
 license           "Apache 2.0"
 description       "Creates \"authorized_keys\" in user \"~/.ssh\" directory from a data bag (encrypted data bag supported)"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.2.7"
+version           "1.2.8"
 
 %w{ubuntu debian redhat centos fedora freebsd}.each do |os|
   supports os
